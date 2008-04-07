7073e5ebecbff5c70f439302be78f4479c5dc91e
Anders Lövgren <andlov@nowise.se>
2008-04-07 01:47:08 +0000

    Adding daemon specific header file (cgpsd.h).

---
+++ cgpsd/cgpsd.h
@@ -0,0 +1,46 @@
+/* Simca-QP predictions for the ChemGPS project.
+ *
+ * Copyright (C) 2007-2008 Anders Lövgren and the Computing Department,
+ * Uppsala Biomedical Centre, Uppsala University.
+ * 
+ * ----------------------------------------------------------------------
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * ----------------------------------------------------------------------
+ *  Contact: Anders Lövgren <anders.lovgren@bmc.uu.se>
+ * ----------------------------------------------------------------------
+ */
+
+#ifndef __CGPSD_H__
+#define __CGPSD_H__
+
+#define CGPSD_QUEUE_LENGTH 50  /* max length for queue of pending connections */
+
+/*
+ * Peer connection endpoint.
+ */
+struct client
+{
+	const struct options *opts;
+	int sock;
+};
+
+void parse_options(int argc, char **argv, struct options *opts);
+void service(struct options *opts);
+void process_peer_request(void *peer);
+int init_socket(struct options *opts);
+
+#endif /* __CGPSD_H__ */
