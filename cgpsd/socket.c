bc61b8167d01fae0b21e74b3b8d677675364365d
Anders Lövgren <andlov@nowise.se>
2008-05-08 00:12:43 +0000

    Fixed declaration of 'opts' shadows a global declaration warning.

--- cgpsd/socket.c
+++ cgpsd/socket.c
@@ -51,17 +51,17 @@
 /*
  * Initilize server socket.
  */
-int init_socket(struct options *opts)
+int init_socket(struct options *popt)
 {
-	if(opts->ipaddr) {
+	if(popt->ipaddr) {
 		struct addrinfo hints, *addr, *next = NULL;
                 char port[6];
                 char host[NI_MAXHOST], serv[NI_MAXSERV];
                 int res;
 		
-                snprintf(port, sizeof(port) - 1, "%d", opts->port);
+                snprintf(port, sizeof(port) - 1, "%d", popt->port);
 		
-		hints.ai_family = opts->family;
+		hints.ai_family = popt->family;
 		hints.ai_socktype = SOCK_STREAM;
 		hints.ai_flags = AI_ADDRCONFIG;
 		hints.ai_protocol = 0;
@@ -69,40 +69,40 @@ int init_socket(struct options *opts)
 		hints.ai_addr = NULL;
 		hints.ai_next = NULL;
 		
-		if(opts->ipaddr == CGPSD_DEFAULT_ADDR) {
+		if(popt->ipaddr == CGPSD_DEFAULT_ADDR) {
 			hints.ai_flags |= AI_PASSIVE;
 			res = getaddrinfo(NULL, port, &hints, &addr);
 		} else {
-			res = getaddrinfo(opts->ipaddr, port, &hints, &addr);
+			res = getaddrinfo(popt->ipaddr, port, &hints, &addr);
 		}
 		if(res != 0) {
 			die("failed resolve %s:%d (%s)",
-			    opts->ipaddr, opts->port, gai_strerror(res));
+			    popt->ipaddr, popt->port, gai_strerror(res));
 		}
 		
 		for(next = addr; next != NULL; next = next->ai_next) {
-			opts->ipsock = socket(next->ai_family, next->ai_socktype, next->ai_protocol);
-			if(opts->ipsock < 0) {
+			popt->ipsock = socket(next->ai_family, next->ai_socktype, next->ai_protocol);
+			if(popt->ipsock < 0) {
 				continue;
 			}
-			res = bind(opts->ipsock, next->ai_addr, next->ai_addrlen);
+			res = bind(popt->ipsock, next->ai_addr, next->ai_addrlen);
 			if(res < 0) {
-				close(opts->ipsock);
+				close(popt->ipsock);
 				continue;
 			}
 			break;
 		}
 		if(!next) {
 			freeaddrinfo(addr);
-			if(opts->ipsock < 0) {
+			if(popt->ipsock < 0) {
 				die("failed create TCP socket");
 			}
 			if(res < 0) {
-				die("failed bind to %s:%d", opts->ipaddr, opts->port);
+				die("failed bind to %s:%d", popt->ipaddr, popt->port);
 			}
 		}
 		
-		if(opts->debug) {
+		if(popt->debug) {
 			if(getnameinfo(next->ai_addr,
 				       next->ai_addrlen,
 				       host, NI_MAXHOST,
@@ -111,41 +111,41 @@ int init_socket(struct options *opts)
 				      host, serv, next->ai_family == AF_INET ? "ipv4" : "ipv6");
 			} else {
 				debug("bind TCP socket to %s port %d (%s)",
-				      opts->ipaddr, opts->port,
+				      popt->ipaddr, popt->port,
 				      next->ai_family == AF_INET ? "ipv4" : "ipv6");
 			}
 		}
 		
 		/* debug("bind TCP socket to %s port %d (%s)",  */
-		/*       opts->ipaddr, opts->port, next->ai_family == AF_INET ? "ipv4" : "ipv6"); */
+		/*       popt->ipaddr, popt->port, next->ai_family == AF_INET ? "ipv4" : "ipv6"); */
 		
-		if(listen(opts->ipsock, opts->backlog) < 0) {
+		if(listen(popt->ipsock, popt->backlog) < 0) {
 			die("failed listen on TCP socket");
 		}
-		debug("successful listen on TCP socket (backlog: %d)", opts->backlog);
+		debug("successful listen on TCP socket (backlog: %d)", popt->backlog);
 	}
-	if(opts->unaddr) {
+	if(popt->unaddr) {
 		struct sockaddr_un sockaddr;
 		
-		opts->unsock = socket(PF_UNIX, SOCK_STREAM, 0);
-		if(opts->unsock < 0) {
+		popt->unsock = socket(PF_UNIX, SOCK_STREAM, 0);
+		if(popt->unsock < 0) {
 			die("failed create UNIX socket");
 		}
 		debug("created UNIX socket");
 		
 		memset(&sockaddr, 0, sizeof(struct sockaddr_un));
 		sockaddr.sun_family = AF_UNIX;
-		strncpy(sockaddr.sun_path, opts->unaddr, sizeof(sockaddr.sun_path));
-		if(bind(opts->unsock, (const struct sockaddr *)&sockaddr,
+		strncpy(sockaddr.sun_path, popt->unaddr, sizeof(sockaddr.sun_path));
+		if(bind(popt->unsock, (const struct sockaddr *)&sockaddr,
 			sizeof(struct sockaddr_un)) < 0) {
 			die("failed bind UNIX socket");
 		}
-		debug("bind UNIX socket to %s", opts->unaddr);
+		debug("bind UNIX socket to %s", popt->unaddr);
 		
-		if(listen(opts->unsock, opts->backlog) < 0) {
+		if(listen(popt->unsock, popt->backlog) < 0) {
 			die("failed listen on UNIX socket");
 		}
-		debug("successful listen on UNIX socket (backlog: %d)", opts->backlog);
+		debug("successful listen on UNIX socket (backlog: %d)", popt->backlog);
 	}
 	return 0;
 }
@@ -153,17 +153,17 @@ int init_socket(struct options *opts)
 /*
  * Close server socket(s).
  */
-void close_socket(struct options *opts)
+void close_socket(struct options *popt)
 {
-	if(opts->ipsock && opts->ipsock != -1) {
-		if(shutdown(opts->ipsock, SHUT_RDWR) < 0) {
+	if(popt->ipsock && popt->ipsock != -1) {
+		if(shutdown(popt->ipsock, SHUT_RDWR) < 0) {
 			logerr("failed close TCP socket");
 		} else {
 			debug("closed TCP socket");
 		}
 	}
-	if(opts->unsock && opts->unsock != -1) {
-		if(shutdown(opts->unsock, SHUT_RDWR) < 0) {
+	if(popt->unsock && popt->unsock != -1) {
+		if(shutdown(popt->unsock, SHUT_RDWR) < 0) {
 			logerr("failed close UNIX socket");
 		} else {
 			debug("closed UNIX socket");
